35f7952d35d89c144391d708e921c499e0602504
anh1124 <ngongoctrunganh@gmail.com>
2024-11-13 16:19:52 +0700

    DONE auto login

--- app/src/main/java/com/example/crabquizz/Scripts/Models/QuestionPack.java
+++ app/src/main/java/com/example/crabquizz/Scripts/Models/QuestionPack.java
@@ -1,11 +1,12 @@
 package com.example.crabquizz.Scripts.Models;
 
-// Model cho bộ câu hỏi
- class QuestionPack {
+public class QuestionPack {
     private int id;
     private String teacherId;
     private String title;
-    private String questionsJson; // JSON string chứa List<Question>
+    private String description;
+    private String topic;
+    private String questionsJson; // JSON string containing List<Question>
 
     // Constructor
     public QuestionPack(int id, String teacherId, String questionsJson) {
@@ -29,6 +30,30 @@ package com.example.crabquizz.Scripts.Models;
 
     public void setTeacherId(String teacherId) {
         this.teacherId = teacherId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getTopic() {
+        return topic;
+    }
+
+    public void setTopic(String topic) {
+        this.topic = topic;
     }
 
     public String getQuestionsJson() {
